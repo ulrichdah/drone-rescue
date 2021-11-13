805e5ffd8b5d2109a4c6be14742ce37f1657304e
ulrichdah <ulrich.dahachinanon@gmail.com>
2021-11-13 20:09:00 +0000

    Update test script and set base parameters

--- sim/argos_simulation/tests.sh
+++ sim/argos_simulation/tests.sh
@@ -1,21 +1,31 @@
 ## declare an array for the test suffixes
-declare -a test_types=("4040_10_" "4040_15_" "4040_20_" "3030_10_" "3030_15_" "3030_20_" "2020_10_" "2020_15_" "2020_20_")
+# declare -a test_types=("4040_10_" "4040_15_" "4040_20_")
+# declare -a test_types=("3030_10_" "3030_15_" "3030_20_")
+declare -a test_types=("2020_10_" "2020_15_" "2020_20_")
 nb_tests=30
 
 for test_type in "${test_types[@]}"
-    
+    # Use the correct file name for the experiment
     sed -i "13s/target_.*/target_"$test_type"random.txt\"/" ../controller/src/argos/buzz_controller_drone_rescue_sim.cpp
     sed -i "14s/relay_.*/target_"$test_type"random.txt\"/" ../controller/src/argos/buzz_controller_drone_rescue_sim.cpp
+    
+    # Use the correct number of robots
     if [[ $test_type == *"_10_"* ]]; then
         sed -i "107s/quantity=\".*\" /quantity=\"10\" /" sparse_sar.argos
-        sed -i "6s/.*/NUMBER_OF_ROBOTS = 15/" config/search_example/parameters.bzz
+        sed -i "6s/.*/NUMBER_OF_ROBOTS = 10/" config/search_example/parameters.bzz
     fi
     if [[ $test_type == *"_15_"* ]]; then
         sed -i "107s/quantity=\".*\" /quantity=\"15\" /" sparse_sar.argos
+        sed -i "6s/.*/NUMBER_OF_ROBOTS = 15/" config/search_example/parameters.bzz
     fi
     if [[ $test_type == *"_20_"* ]]; then
         sed -i "107s/quantity=\".*\" /quantity=\"20\" /" sparse_sar.argos
+        sed -i "6s/.*/NUMBER_OF_ROBOTS = 20/" config/search_example/parameters.bzz
     fi
+
+    # Build
+    cd ../controller/build/ && make clean && make && make install && cd ../../argos_simulation && bzzc sparse_sar.bzz && bzzc target_drift_example.bzz
+
     echo "========= TEST $test_type ========="
     for i in $(seq $nb_tests); do
         echo '----' >> /home/docker/drone-rescue/sim/controller/src/argos/results/relay_"$test_type"random.txt
